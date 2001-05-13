86e43037e89fff1ae78859ba7643486d9ea67753
elias <elias@dbe00209-9f24-42c9-b76a-50cd5f11d5da>
2001-05-13 14:26:35 +0000

    the new makefiles

---
+++ gnelib/fixming.bat
@@ -0,0 +1,7 @@
+@copy /Y makefile.mingw makefile.os
+@echo Setting up mingw makefile. Some common commands are:
+@echo make           to compile the lib and all the examples.
+@echo make install   to install the library on your OS.
+@echo make clean     use this first when upgrading from an older version.
+@echo make lib       if you don't need the examples.
+@echo make uninstall to uninstall the library again.
