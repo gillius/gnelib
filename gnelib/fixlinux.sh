89ccf33cb59811da1976421d219dac2cb913e9cf
elias <elias@dbe00209-9f24-42c9-b76a-50cd5f11d5da>
2001-10-28 21:06:07 +0000

    added quotes

--- gnelib/fixlinux.sh
+++ gnelib/fixlinux.sh
@@ -1,7 +1,7 @@
 cp -f makefile.linux makefile.os
-echo Setting up linux makefile. Some common commands are:
-echo make           to compile the lib and all the examples.
-echo make install   to install the library on your OS.
-echo make clean     use this first when upgrading from an older version.
-echo make lib       if you don't need the examples.
-echo make uninstall to uninstall the library again.
+echo "Setting up linux makefile. Some common commands are:"
+echo "make           to compile the lib and all the examples."
+echo "make install   to install the library on your OS."
+echo "make clean     use this first when upgrading from an older version."
+echo "make lib       if you don't need the examples."
+echo "make uninstall to uninstall the library again."
