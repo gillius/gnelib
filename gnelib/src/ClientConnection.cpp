5a16103b76738e07d1cc9754ebdb3852b145a12d
Jason Winnebeck <gillius@users.noreply.github.com>
2001-11-18 06:53:37 +0000

    Indent and warning fix.

--- gnelib/src/ClientConnection.cpp
+++ gnelib/src/ClientConnection.cpp
@@ -28,13 +28,13 @@ namespace GNE {
 
 //##ModelId=3B075380037F
 ClientConnection::ClientConnection(int outRate, int inRate, ConnectionListener* listener)
-: Connection(outRate, inRate, listener) {
-	gnedbgo(5, "created");
+  : Connection(outRate, inRate, listener) {
+  gnedbgo(5, "created");
 }
 
-//##ModelId=3B07538003B8
+  //##ModelId=3B07538003B8
 ClientConnection::~ClientConnection() {
-	gnedbgo(5, "destroyed");
+  gnedbgo(5, "destroyed");
 }
 
 /**
@@ -42,50 +42,51 @@ ClientConnection::~ClientConnection() {
  */
 //##ModelId=3B07538003BA
 void ClientConnection::run() {
-	gnedbgo1(1, "Trying to connect to %s", address.toString().c_str());
-  NLboolean check = nlConnect(sockets.r, &address.getAddress());
+  gnedbgo1(1, "Trying to connect to %s", address.toString().c_str());
+  NLaddress temp = address.getAddress();
+  NLboolean check = nlConnect(sockets.r, &temp);
   if (check == NL_TRUE) {
-		gnedbgo2(2, "connection r: %i, u: %i", sockets.r, sockets.u);
-		assert(eventListener != NULL);
-		try {
-			SyncConnection sync(this);
-			reg(true, false);
-			ps->start();
-			connecting = true;
-			eventListener->onConnect(&sync); //SyncConnection will relay this
-			sync.release();
-			connected = true;
-			connecting = false;
-		} catch (Error e) {
-			//The client should catch any errors in onConnect, but release can
-			//give an error, so we catch it.
-			gnedbgo1(1, "Connection failure: %s", e.toString().c_str());
-			eventListener->onConnectFailure(e);
-		}
+    gnedbgo2(2, "connection r: %i, u: %i", sockets.r, sockets.u);
+    assert(eventListener != NULL);
+    try {
+      SyncConnection sync(this);
+      reg(true, false);
+      ps->start();
+      connecting = true;
+      eventListener->onConnect(&sync); //SyncConnection will relay this
+      sync.release();
+      connected = true;
+      connecting = false;
+    } catch (Error e) {
+      //The client should catch any errors in onConnect, but release can
+      //give an error, so we catch it.
+      gnedbgo1(1, "Connection failure: %s", e.toString().c_str());
+      eventListener->onConnectFailure(e);
+    }
   } else {
-		assert(eventListener != NULL);
-		Error err = Error::createLowLevelError();
-		err.setCode(Error::ConnectionTimeOut);
-		gnedbgo1(1, "Connection failure: %s", err.toString().c_str());
+    assert(eventListener != NULL);
+    Error err = Error::createLowLevelError();
+    err.setCode(Error::ConnectionTimeOut);
+    gnedbgo1(1, "Connection failure: %s", err.toString().c_str());
     eventListener->onConnectFailure(err);
   }
 }
 
 //##ModelId=3B07538003BB
 bool ClientConnection::open(const Address& dest, int localPort) {
-	if (!dest)
-		return true;
-	else {
-		address = dest;
-		sockets.r = nlOpen(localPort, NL_RELIABLE_PACKETS);
-		return (sockets.r == NL_INVALID);
-	}
+  if (!dest)
+    return true;
+  else {
+    address = dest;
+    sockets.r = nlOpen(localPort, NL_RELIABLE_PACKETS);
+    return (sockets.r == NL_INVALID);
+  }
 }
 
 //##ModelId=3B07538003C1
 void ClientConnection::connect() {
-	assert(sockets.r != NL_INVALID);
-	assert(address);
+  assert(sockets.r != NL_INVALID);
+  assert(address);
   start();
 }
 
